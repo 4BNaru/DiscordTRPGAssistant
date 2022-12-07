6e0470df3b53f39cacc561866632757fb2cfd05c
4B_NARU <tav137@naver.com>
2022-12-08 03:19:32 +0900

    file structure fix, docs, etc

--- Script/Assistant.py
+++ Script/Assistant.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from case_group import case_group
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -8,5 +9,9 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 @bot.command()
 async def 어시스턴트(callbot):
     await callbot.send('네')
+
+@bot.command()
+async def 게임설정(asdfasdf):
+    await asdfasdf.send('게임 설정을 시작합니다.')
 
 bot.run('MTA1MDA0MDMwNDcwMDQ4OTc0OA.GWEj08.QVUODXc9XeA2mMOB7dqCncBilGMiRVEXXEOQFo')
